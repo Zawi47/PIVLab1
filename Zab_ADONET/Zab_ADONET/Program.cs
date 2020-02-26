d9b262f9588f61d25fd65d5f09aacc355182e1df
Zawi47 <zawislak.jakub2@gmail.com>
2020-02-26 14:52:27 +0100

    Add files via upload

---
+++ Zab_ADONET/Zab_ADONET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zab_ADONET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
