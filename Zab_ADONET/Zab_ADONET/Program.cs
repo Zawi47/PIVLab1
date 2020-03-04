43dfe0180c99a1b5b6799dfbd88400b34a60926d
Zawi47 <zawislak.jakub2@gmail.com>
2020-03-04 09:41:25 +0100

    Add files via upload

---
+++ Zab_ADONET/Zab_ADONET/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Zab_ADONET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var db = new DB();
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+            db.Select(connection);
+            //db.Insert(connection, 5, "Bielsko");
+            db.Delete(connection);
+            connection.Close();
+            Console.ReadKey();
+        }
+    }
+}
